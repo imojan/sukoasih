cc4bd12c2b6972dcc2f8426868e1d03113828673
Macbook Pro <macbookpro@saZan.local>
2025-07-25 00:38:23 +0700

    tambah ht.access

--- koneksi.php
+++ koneksi.php
@@ -3,3 +3,14 @@
 $koneksi = mysqli_connect("localhost", "root", "", "klinik");
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
